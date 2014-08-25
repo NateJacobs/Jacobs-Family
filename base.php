a494558c2a7f47aa4f2f3f610ce48b7805bbdd81
Nate Jacobs <nate@natejacobs.org>
2014-08-24 23:15:26 -0700

    Don't display the sidebar if it is the genealogy pages.

--- base.php
+++ base.php
@@ -17,7 +17,7 @@
       <main class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
-      <?php if (roots_display_sidebar()) : ?>
+      <?php if (roots_display_sidebar() && !get_query_var('tng_type')) : ?>
         <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
           <?php include roots_sidebar_path(); ?>
         </aside><!-- /.sidebar -->
